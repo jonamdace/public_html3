771bed5cc5ef38c356ae31a3cff28ab56978826b
Manoj <jonamdace@gmail.com>
2018-09-01 21:07:17 +0530

    issue solved

--- Platform/Templates/HomeScreen.js
+++ Platform/Templates/HomeScreen.js
@@ -1,6 +1,6 @@
 'use strict';
 import React, {Component, PropTypes} from "react";
-import {View, StyleSheet, Animated, Text, TextInput, ScrollView, Dimensions, TouchableOpacity, AsyncStorage} from "react-native";
+import { View, StyleSheet, Animated, Text, TextInput, ScrollView, Dimensions, TouchableOpacity, AsyncStorage, PermissionsAndroid, BackAndroid } from "react-native";
 
 import CommonStyle from "../Styles/CommonStyle";
 import MKButton from "../Component/MKButton";
@@ -23,50 +23,80 @@ export default class HomeScreen extends Component {
 		this.navigate=this.props.navigateTo;
 	}
 
+	async requestPermission() {
+		var grantedBool = false;
+		try {
+		const granted = await PermissionsAndroid.requestMultiple([
+			PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+			PermissionsAndroid.PERMISSIONS.CAMERA,
+			PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION
+		]);
+
+		if(granted["android.permission.ACCESS_FINE_LOCATION"] == "granted" && granted["android.permission.CAMERA"] == "granted" && granted["android.permission.ACCESS_COARSE_LOCATION"] == "granted"){
+			grantedBool = true;
+		}
+		
+		//alert(JSON.stringify(granted) +  grantedBool);
+
+
+		} catch (err) {
+			grantedBool = false;
+		}
+		return grantedBool;
+	}
+
 	async componentDidMount() {
 		var that = this;
-		var username = await AsyncStorage.getItem("username");
-		var password = await AsyncStorage.getItem("password");
-		
-		if(username != null && password != null){
-			var postJson = new FormData();
-			postJson.append("username", username);
-			postJson.append("password", password);
-			var subUrl = "getLoginFromApps";
+		var resp = await that.requestPermission();
+		if(resp){
+			var username = await AsyncStorage.getItem("username");
+			var password = await AsyncStorage.getItem("password");	
+			if(username != null && password != null){
+				var postJson = new FormData();
+				postJson.append("username", username);
+				postJson.append("password", password);
+				var subUrl = "getLoginFromApps";
 
-			that.props.updateLoading(true);
-			var response = await doPost(subUrl, postJson);
-			if(response != null){				
-				var active = response['active'];
-				var name = response['name'];
-				var lastlogin = response['lastlogin']; 
-				var img = response['img'];
-				if(active!=null && (active == "active" ||  active == "InActive")){
+				that.props.updateLoading(true);
+				var response = await doPost(subUrl, postJson);
+				if(response != null){				
+					var active = response['active'];
+					var name = response['name'];
+					var lastlogin = response['lastlogin']; 
+					var img = response['img'];
+					if(active!=null && (active == "active" ||  active == "InActive")){
 
-					var userid = response['userid'];
-					var userCode = response['userCode'];
+						var userid = response['userid'];
+						var userCode = response['userCode'];
 
-					await AsyncStorage.setItem('userid', userid);
-					await AsyncStorage.setItem('userCode', userCode);
-					await AsyncStorage.setItem('active', active);
-					await AsyncStorage.setItem('name', name);
-					await AsyncStorage.setItem('lastlogin', lastlogin);
-					await AsyncStorage.setItem('img', img);
+						await AsyncStorage.setItem('userid', userid);
+						await AsyncStorage.setItem('userCode', userCode);
+						await AsyncStorage.setItem('active', active);
+						await AsyncStorage.setItem('name', name);
+						await AsyncStorage.setItem('lastlogin', lastlogin);
+						await AsyncStorage.setItem('img', img);
 
-					that.props.updateLoading(false);
+						that.props.updateLoading(false);
 
-					that.onPressRedirect("Dashboard");
+						that.onPressRedirect("Dashboard");
 
-				} else if(active == "InActive"){
-					alert("Your Profile was not activated!");
-				} else {
-					alert("Username/Password is incorrect");
+					} else if(active == "InActive"){
+						alert("Your Profile was not activated!");
+					} else {
+						alert("Username/Password is incorrect");
+					}
 				}
+				that.props.updateLoading(false);
 			}
-			that.props.updateLoading(false);
+		} else {
+			this.handleBackButtonClick();
 		}
 	}
 
+	handleBackButtonClick() {
+		BackAndroid.exitApp();
+		return true;
+	}
 	onPressRedirect(routes){
 		this.navigate(routes);
 	}
